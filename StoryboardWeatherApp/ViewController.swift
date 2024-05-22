ad69c2abc6b5c4a73b0446db874492616bac6910
wglee0511 <ranazang@naver.com>
2024-05-22 18:28:12 +0900

    feature: random number

--- StoryboardWeatherApp/ViewController.swift
+++ StoryboardWeatherApp/ViewController.swift
@@ -18,10 +18,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let weatherValue = WeatherType.비
-        let tempValue = 11
-        
+        let weatherValue = [WeatherType.비, WeatherType.눈, WeatherType.맑음, WeatherType.흐림].randomElement() ?? WeatherType.맑음
+        let tempValue = Int.random(in: -50 ... 50)
         let imageValue = getWeatherImage(weatherValue: weatherValue, imageView: weatherImageValue)
+        
         weatherImageValue.image = imageValue
         weatherTextValue.text = "\(weatherValue)"
         weatherImageBackgroundImageValue.image = UIImage(named: "image_seoul")
